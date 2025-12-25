70d96b039789382c342e1bfe0e074b0ee17e39e0
Tuan Le <letuanvl03@gmail.com>
2025-12-25 12:13:12 +0700

    feat: Add student import and export functionality

--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,16 @@ Rails.application.routes.draw do
     end
   end
   resources :students, param: :ma_sv do
+    collection do
+      get  :import, action: :import_new      # /students/import
+      post :import, action: :import_create   # /students/import
+      get  :export                           # /students/export
+      get  :download_template                # /students/download_template
+    end
+
     scope module: :students do
       resources :diem_hoc_taps
       resources :diem_ren_luyens, only: :index
     end
   end
-
-  get  "/bang-diem/new", to: "grades#new",    as: "new_grade"
-  post "/bang-diem",     to: "grades#create", as: "create_grade"
 end
